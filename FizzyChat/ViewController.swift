eadbb4a234183668b5bf4f553d9c7d86a9126449
Farzad <farzad@farzads-macbook-pro.local>
2018-08-19 00:50:06 -0700

    Initial Commit

---
+++ FizzyChat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FizzyChat
+//
+//  Created by Farzad on 8/19/18.
+//  Copyright © 2018 Farzad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
